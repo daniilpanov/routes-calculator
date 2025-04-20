11a86ba061c6b4a719e2b64a4fa963017e4ee0a2
Daniil Panov <daniil_panov2005@mail.ru>
2025-04-20 20:46:45 +0300

    feat[src]: add conversion

--- src/main.py
+++ src/main.py
@@ -26,6 +26,34 @@ def union_country_and_name(country, name):
     return name or country
 
 
+class Converter:
+    @classmethod
+    def create_with_ru(cls, conversion_rates, plus_percents=0):
+        inst = cls(conversion_rates, plus_percents)
+        inst.conversion_rates['RUB'] = 1
+        inst.conversion_rates['RUR'] = 1
+        return inst
+
+    def __init__(self, conversion_rates, plus_percents=0):
+        self.conversion_rates = conversion_rates
+        self.plus_percents = plus_percents
+
+    def _convert(self, value, from_currency, to_currency):
+        value = float(value)
+        value += value * self.plus_percents / 100
+        return value * self.conversion_rates[from_currency] / self.conversion_rates[to_currency]
+
+    def recursive_currency_convertion(self, obj, needle_currency):
+        if isinstance(obj, (list, tuple, set)):
+            return [self.recursive_currency_convertion(item, needle_currency) for item in obj]
+        elif isinstance(obj, dict):
+            if 'Currency' in obj and 'Price' in obj:
+                obj['Price'] = self._convert(obj['Price'], obj['Currency'], needle_currency)
+                obj['Currency'] = needle_currency
+            return dict((i, self.recursive_currency_convertion(v, needle_currency)) for i, v in obj.items())
+        return obj
+
+
 @app.get('/get_departures')
 async def all_departure_by_date(date: datetime.date):
     prepared_from = dict()
@@ -46,12 +74,16 @@ async def all_destination_by_date(date: datetime.date, departure_point_id: str):
     return prepared_to
 
 
+def get_rates(dt_now):
+    rates = ExchangeRates(dt_now)
+    return {currency.code: float(currency.value) for currency in rates.rates}
+
+
 @app.get('/')
 async def home(request: Request):
     dt_now = datetime.datetime.now()
-    rates = ExchangeRates(dt_now)
-    parsed_rates = {currency.code: float(currency.value) for currency in rates.rates}
     points_from = await all_departure_by_date(dt_now.date())
+    parsed_rates = get_rates(dt_now)
     return templates.TemplateResponse('routes-calc-form.html', {
         'request': request,
         'rates': dict(parsed_rates),
@@ -67,6 +99,8 @@ async def calculate(request: CalculateFormRequest):
     if not container_id:
         raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail='No container found')
     try:
-        return await fesco.get_routes(dtc, request.departureId, request.destinationId, container_id, 'ru')
+        routes = await fesco.get_routes(dtc, request.departureId, request.destinationId, container_id, 'ru')
+        parsed_rates = get_rates(datetime.datetime.combine(dtc, datetime.time()))
+        return Converter.create_with_ru(parsed_rates).recursive_currency_convertion(routes, request.currency)
     except Exception as e:
         raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)) from e
