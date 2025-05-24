83ecd4a3f14852a4c01498026875d8ae42623f7a
Daniel <daniil_panov2005@mail.ru>
2025-05-24 23:06:33 +0300

    Revert "Hotfix/backend port"

--- src/api/custom/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .container import ContainerModel, ContainerType
-from .company import CompanyModel
-from .point import PointModel, PointType
-from .route import RouteModel
-from .price import PriceModel, PriceType
