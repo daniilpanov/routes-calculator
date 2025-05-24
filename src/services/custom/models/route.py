9fe0b1e80adcdfad6d451bc3540732709f6c00ea
Daniil Panov <daniil_panov2005@mail.ru>
2025-05-24 23:19:55 +0300

    feat[services/custom]: add models module

---
+++ src/services/custom/models/route.py
@@ -0,0 +1,66 @@
+# CompanyID: ..., ContainerID: ..., PointID: ..., Price: <number>, PriceType: <simple/fifo/filo>; etc.
+import datetime
+
+from sqlalchemy import ForeignKey, UniqueConstraint, DateTime
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from typing import Optional
+
+from src.database import Base
+from . import CompanyModel, ContainerModel
+
+from .point import PointModel
+
+
+class SeaRouteModel(Base):
+    uid = ('company_id', 'container_id', 'start_point_id', 'end_point_id', 'effective_from', 'effective_to')
+
+    __tablename__ = 'sea_routes'
+    __table_args__ = (
+        UniqueConstraint(*uid),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    company_id: Mapped[int] = mapped_column(ForeignKey('companies.id'))
+    container_id: Mapped[int] = mapped_column(ForeignKey('containers.id'))
+    start_point_id: Mapped[int] = mapped_column(ForeignKey('points.id'))
+    end_point_id: Mapped[int] = mapped_column(ForeignKey('points.id'))
+    effective_from: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    effective_to: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+
+    fifo: Mapped[float] = mapped_column()
+    filo: Mapped[Optional[float]] = mapped_column()
+
+    start_point: Mapped[PointModel] = relationship(PointModel, foreign_keys=[start_point_id])
+    end_point: Mapped[PointModel] = relationship(PointModel, foreign_keys=[end_point_id])
+    company: Mapped[CompanyModel] = relationship()
+    container: Mapped[ContainerModel] = relationship()
+
+    @property
+    def price(self):
+        return self.filo or self.fifo
+
+
+class RailRouteModel(Base):
+    uid = ('company_id', 'container_id', 'start_point_id', 'end_point_id', 'effective_from', 'effective_to')
+
+    __tablename__ = 'rail_routes'
+    __table_args__ = (
+        UniqueConstraint(*uid),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    company_id: Mapped[int] = mapped_column(ForeignKey('companies.id'))
+    container_id: Mapped[int] = mapped_column(ForeignKey('containers.id'))
+    start_point_id: Mapped[int] = mapped_column(ForeignKey('points.id'))
+    end_point_id: Mapped[int] = mapped_column(ForeignKey('points.id'))
+    effective_from: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    effective_to: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+
+    price: Mapped[float] = mapped_column()
+    drop: Mapped[float] = mapped_column()
+    guard: Mapped[Optional[float]] = mapped_column()
+
+    start_point: Mapped[PointModel] = relationship(PointModel, foreign_keys=[start_point_id])
+    end_point: Mapped[PointModel] = relationship(PointModel, foreign_keys=[end_point_id])
+    company: Mapped[CompanyModel] = relationship()
+    container: Mapped[ContainerModel] = relationship()
