554e58a4fa3c0daaa8244f04c3068172ff3c021a
Daniil Panov <daniil_panov2005@mail.ru>
2025-06-01 20:08:04 +0300

    fix[src/main]: use union_country_and_name

--- src/main.py
+++ src/main.py
@@ -73,7 +73,7 @@ async def all_destination_by_date(date: datetime.date, departure_point_id: str):
     prepared_to = dict()
     # from FESCO
     data_to = await fesco.get_destination_points_by_date(date, departure_point_id, 'ru')
-    prepared_to.update(map(lambda x: (x['country'] + ', ' + x['name'], x['id']), data_to))
+    prepared_to.update(map(lambda x: (union_country_and_name(x.get('country'), x.get('name')), x['id']), data_to))
 
     return prepared_to
 
