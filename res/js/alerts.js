b0b11dd773642c42bbd27d6d44b9ac374b8d24b7
Daniil Panov <daniil_panov2005@mail.ru>
2025-06-15 23:54:55 +0300

    feat[js/(alerts+rates)]: upgrade

--- res/js/alerts.js
+++ res/js/alerts.js
@@ -1,6 +1,6 @@
-function showGlobalAlert(message) {
+function showGlobalAlert(message, type = 'danger') {
     const container = document.getElementById('alertContainer');
-    container.innerHTML = `<div class="alert alert-danger alert-fixed alert-dismissible fade show" role="alert">
+    container.innerHTML = `<div class="alert alert-${type} alert-fixed alert-dismissible fade show" role="alert">
       ${message}
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Закрыть"></button>
     </div>`;
@@ -10,4 +10,5 @@ function showGlobalAlert(message) {
         const alert = container.querySelector('.alert');
         if (alert) new bootstrap.Alert(alert).close();
     }, 3000);
+    console.log(type, '-', message);
 }
