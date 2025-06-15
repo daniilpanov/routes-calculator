1ce93d77e7d86de57977fe714ce017790b19accb
Daniil Panov <daniil_panov2005@mail.ru>
2025-06-15 12:09:18 +0300

    feat[services/custom]: create data mappers

---
+++ src/services/custom/mappers/points.py
@@ -0,0 +1,11 @@
+def _map_point(point):
+    return {
+        'id': point.id,
+        'company': 'HUB',  # TODO: replace with primary-join with routes
+        'country': point.RU_country,
+        'name': point.RU_city,
+    }
+
+
+def map_points(points):
+    return map(_map_point, points)
