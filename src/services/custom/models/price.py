2938176108c8058fde5ca6cb826a02a071d4724d
qurlya <pingvining05@mail.ru>
2025-07-23 10:47:20 +0300

    src/services/custom/models/price : created table "price_types"

--- src/services/custom/models/price.py
+++ src/services/custom/models/price.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String
+from sqlalchemy import String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -10,3 +10,15 @@ class СurrencyModel(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
     currency_name: Mapped[str] = mapped_column(String(30), unique=True)
+
+
+class PriceTypeModel(Base):
+    uid = ("name",)
+
+    __tablename__ = "price_types"
+    __table_args__ = (UniqueConstraint(*uid),)
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    name: Mapped[str] = mapped_column(String(30))
+    need_drop: Mapped[bool] = mapped_column()
+    direction: Mapped[int] = mapped_column()
