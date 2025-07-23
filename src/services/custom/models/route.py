95eb238f9ee455e96dc06feda638c9e7a8461ece
qurlya <pingvining05@mail.ru>
2025-07-23 10:50:52 +0300

    src/services/custom/models/route : created bd "routes"

--- src/services/custom/models/route.py
+++ src/services/custom/models/route.py
@@ -79,3 +79,33 @@ class RailRouteModel(Base):
     )
     company: Mapped[CompanyModel] = relationship()
     container: Mapped[ContainerModel] = relationship()
+
+
+class RouteModel(Base):
+    uid = (
+        "company_id",
+        "container_id",
+        "start_point_id",
+        "end_point_id",
+        "effective_from",
+        "effective_to",
+    )
+    __tablename__ = "routes"
+    __table_args__ = (UniqueConstraint(*uid),)
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"))
+    container_id: Mapped[int] = mapped_column(ForeignKey("containers.id"))
+    start_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"))
+    end_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"))
+    effective_from: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    effective_to: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+
+    start_point: Mapped[PointModel] = relationship(
+        PointModel, foreign_keys=[start_point_id]
+    )
+    end_point: Mapped[PointModel] = relationship(
+        PointModel, foreign_keys=[end_point_id]
+    )
+    company: Mapped[CompanyModel] = relationship()
+    container: Mapped[ContainerModel] = relationship()
