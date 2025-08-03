611fb745db13b409aa2ef2e1c2b13dec072a5dbe
qurlya <pingvining05@mail.ru>
2025-08-03 18:35:31 +0300

    services/custom/mappers/points: update method

--- src/services/custom/mappers/points.py
+++ src/services/custom/mappers/points.py
@@ -1,11 +1,11 @@
-def _map_point(point):
+def _map_point(point_with_company_name):
+    point, company_name = point_with_company_name
     return {
         "id": point.id,
-        "company": point.company_name,
+        "company": company_name,
         "country": point.RU_country,
         "name": point.RU_city,
     }
 
-
-def map_points(points):
-    return map(_map_point, points)
+def map_points(points_with_company_name):
+    return map(_map_point, points_with_company_name)
