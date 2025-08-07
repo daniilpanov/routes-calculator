d964e25704ccccf03c3e2c9df1629de6c17838ef
max1m111an <maksimilianbegunov@inbox.ru>
2025-08-07 18:07:23 +0300

    services/custom/models/point: Changed PointModel;  to connect with aliases; added PointAliasesModel

--- src/services/custom/models/point.py
+++ src/services/custom/models/point.py
@@ -1,17 +1,51 @@
-from sqlalchemy import String, UniqueConstraint
-from sqlalchemy.orm import Mapped, mapped_column
+import enum
+
+from sqlalchemy import Boolean, Enum, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.database import Base
 
 
 class PointModel(Base):
-    uid = ("city", "country")
-
     __tablename__ = "points"
-    __table_args__ = (UniqueConstraint(*uid),)
 
     id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
-    city: Mapped[str] = mapped_column(String(100))
     country: Mapped[str] = mapped_column(String(50))
-    RU_city: Mapped[str | None] = mapped_column(String(100), nullable=True)
     RU_country: Mapped[str | None] = mapped_column(String(50), nullable=True)
+
+    # Connect with aliases
+    aliases: Mapped[list["PointAliasesModel"]] = relationship(
+        "PointAliasesModel",
+        back_populates="point",
+        cascade="all, delete-orphan",
+        lazy="selectin",
+    )
+
+
+class LangType(enum.Enum):
+    RU = "RU"
+    EN = "EN"
+
+
+class PointAliasesModel(Base):
+    __tablename__ = "aliases"
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    is_main: Mapped[bool] = mapped_column(Boolean, default=False)
+    alias_name: Mapped[str] = mapped_column(String(25), nullable=False, unique=True)
+
+    # Connect with points
+    point_id: Mapped[str] = mapped_column(Integer, ForeignKey("points.id", ondelete="CASCADE"))
+    point: Mapped["PointModel"] = relationship(
+        "PointModel",
+        back_populates="aliases",
+    )
+
+    lang: Mapped[LangType] = mapped_column(
+        Enum(
+            LangType,
+            create_constraint=True,
+            check_constraint=True,
+            validate_strings=True,
+        )
+    )
