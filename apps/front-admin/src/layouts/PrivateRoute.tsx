ffc1f68697d2e5523204aa645467b061412d4801
Daniel <daniil_panov2005@mail.ru>
2025-08-23 01:10:28 +0300

    fix[front-admin/layouts]: redirect to login page ib PrivateRoute
    /login -> /admin/login

--- apps/front-admin/src/layouts/PrivateRoute.tsx
+++ apps/front-admin/src/layouts/PrivateRoute.tsx
@@ -12,5 +12,5 @@ export default function PrivateRoute() {
         }
     }, []);
 
-    return currentUser ? <Outlet /> : <Navigate to="/login" replace />;
+    return currentUser ? <Outlet /> : <Navigate to="/admin/login" replace />;
 }
