e3f63e468907fc78bf11f69373df02333aeefd1b
max1m111an <maksimilianbegunov@inbox.ru>
2025-08-22 16:06:13 +0300

    backend/services/custom/point&routes: Union SQL-queries; to load labels for 'country' and 'city' via 'aliased'; to load PointModel.parent

--- apps/backend/services/custom/points.py
+++ apps/backend/services/custom/points.py
@@ -4,16 +4,28 @@ from functools import partial
 from backend.database import database
 from backend.mapper_decorator import apply_mapper
 from sqlalchemy import select
+from sqlalchemy.orm import aliased
 
 from .mappers.points import map_points
 from .models import CompanyModel, PointModel, RailRouteModel, SeaRouteModel
+from .models.point import LangType, PointAliasModel
 
 
 def _build_stmt(date, route_class, id_field):
+    CityRuAlias = aliased(PointAliasModel)
+    CityEnAlias = aliased(PointAliasModel)
+    pointCountry = aliased(PointModel)
+    CountryRuAlias = aliased(PointAliasModel)
+    CountryEnAlias = aliased(PointAliasModel)
+
     return (  # noqa: ECE001
         select(
             PointModel,
             CompanyModel.name.label("company_name"),
+            CityEnAlias.alias_name.label("city"),
+            CityRuAlias.alias_name.label("RU_city"),
+            CountryEnAlias.alias_name.label("country"),
+            CountryRuAlias.alias_name.label("RU_country"),
         )
         .distinct()
         .join(
@@ -23,6 +35,35 @@ def _build_stmt(date, route_class, id_field):
             & (route_class.effective_to >= date),
         )
         .join(CompanyModel)
+        .join(
+            CityEnAlias,
+            (CityEnAlias.point_id == PointModel.id)
+            & (CityEnAlias.lang == LangType.EN)
+            & (CityEnAlias.is_main == 1),
+        )
+        .join(
+            CityRuAlias,
+            (CityRuAlias.point_id == PointModel.id)
+            & (CityRuAlias.lang == LangType.RU)
+            & (CityRuAlias.is_main == 1),
+        )
+        .join(
+            pointCountry,
+            (PointModel.parent_id == pointCountry.id)
+            & (pointCountry.parent_id.is_(None)),
+        )
+        .join(
+            CountryEnAlias,
+            (CountryEnAlias.point_id == pointCountry.id)
+            & (CountryEnAlias.lang == LangType.EN)
+            & (CountryEnAlias.is_main == 1),
+        )
+        .join(
+            CountryRuAlias,
+            (CountryRuAlias.point_id == pointCountry.id)
+            & (CountryRuAlias.lang == LangType.RU)
+            & (CountryRuAlias.is_main == 1),
+        )
     )
 
 
