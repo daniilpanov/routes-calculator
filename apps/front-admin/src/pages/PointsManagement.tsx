39ff626780850bb66ebae241138bc3d1dfcabe40
Qurlya <pingvining05@mail.ru>
2025-08-24 16:16:58 +0300

    front-admin/pages: added PointsManagement page

---
+++ apps/front-admin/src/pages/PointsManagement.tsx
@@ -0,0 +1,8 @@
+export default function PointsManagement() {
+    return (
+        <div className="in_development">
+            <p>Страница: Points management ('points_management').</p>
+            <p>Статус: в разработке.</p>
+        </div>
+    );
+}
