67100fb37c1e19d35f37524d89fe17845a342481
Qurlya <pingvining05@mail.ru>
2025-08-24 16:16:01 +0300

    front-admin/pages: add DataImport page

---
+++ apps/front-admin/src/pages/DataImport.tsx
@@ -0,0 +1,8 @@
+export default function DataImport() {
+    return (
+        <div className="in_development">
+            <p>Страница: Data import ('/data_import').</p>
+            <p>Статус: в разработке.</p>
+        </div>
+    );
+}
