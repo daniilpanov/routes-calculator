91edbc1e69dd50aafae435d6e120979b27e1f64a
max1m111an <maksimilianbegunov@inbox.ru>
2025-09-09 20:50:18 +0300

    back_admin: beta3
    Make CRUD for /points
    add cyr-lat check in /find_name
    add points pagination

--- apps/back_admin/models/requests/point.py
+++ apps/back_admin/models/requests/point.py
@@ -6,3 +6,16 @@ class AddPointModelRequest(BaseModel):
     country: str
     RU_city: str
     RU_country: str
+
+
+class EditPointRequest(BaseModel):
+    point_id: int
+    city: str | None = None
+    country: str | None = None
+    RU_city: str | None = None
+    RU_country: str | None = None
+
+
+class FilterPointRequest(BaseModel):
+    page: int = 1
+    limit: int = 25
