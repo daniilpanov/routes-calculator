6fcf1437f7cc6c278ef214322c727f8cbfeed392
max1m111an <maksimilianbegunov@inbox.ru>
2025-09-11 23:01:57 +0300

    back_admin: beta1
    fix /edit & /add

--- apps/back_admin/api/routes/post.py
+++ apps/back_admin/api/routes/post.py
@@ -1,16 +1,16 @@
-import pandas as pd
+from datetime import datetime
+
 from fastapi import APIRouter, HTTPException
 
 from back_admin.database import database
-from back_admin.models import (
-    RailRouteModel,
-    SeaRouteModel,
-)
+from back_admin.models import RailRouteModel, SeaRouteModel
 from back_admin.models.requests.route import (
+    AddRouteRequest,
+    DeleteRoutesRequest,
     EditRouteRequest,
-    FilterRoutesRequest, AddRouteRequest, DeleteRoutesRequest,
+    FilterRoutesRequest,
 )
-from sqlalchemy import delete, func, select, update
+from sqlalchemy import and_, delete, func, not_, or_, select, update
 from sqlalchemy.exc import InvalidRequestError
 from sqlalchemy.orm import joinedload
 
@@ -24,14 +24,7 @@ async def exe_q(q, return_scalar=False):
 
 
 def parse_date(date_value: str):
-    if pd.isna(date_value):
-        return None
-    if isinstance(date_value, str):
-        try:
-            return pd.to_datetime(date_value, dayfirst=True).date()
-        except:
-            return None
-    return pd.to_datetime(date_value).date()
+    return datetime.strptime(date_value, "%d.%m.%Y").date()
 
 
 def get_route_class(_type: str):
@@ -66,7 +59,7 @@ def parseRoute(_route, _type):
             },
             {
                 "type": "price",
-                "value": getattr(_route, "price", None) if not hasattr(_route, "filo") else None,
+                "value": getattr(_route, "price", None) if _type == "rail" else None,
                 "currency": "RUB",
             },
             {
@@ -118,26 +111,22 @@ async def getRoutes(_filter: FilterRoutesRequest):
             )
 
     # Dates filter
-    if _filter.filter_fields.effective_from:
+    if _filter.filter_fields.effective_from and _filter.filter_fields.effective_to:
         date_from = parse_date(_filter.filter_fields.effective_from)
-
-        rail_stmt = rail_stmt.where(
-            RailRouteModel.effective_from >= date_from,
-        )
-
-        sea_stmt = sea_stmt.where(
-            SeaRouteModel.effective_from >= date_from,
-        )
-
-    if _filter.filter_fields.effective_to:
         date_to = parse_date(_filter.filter_fields.effective_to)
 
         rail_stmt = rail_stmt.where(
-            RailRouteModel.effective_to <= date_to,
+            not_(or_(and_(RailRouteModel.effective_from < date_from,
+                          RailRouteModel.effective_to < date_from),
+                     and_(RailRouteModel.effective_from > date_to,
+                          RailRouteModel.effective_to > date_to))),
         )
 
         sea_stmt = sea_stmt.where(
-            SeaRouteModel.effective_to <= date_to,
+            not_(or_(and_(SeaRouteModel.effective_from < date_from,
+                          SeaRouteModel.effective_to < date_from),
+                     and_(SeaRouteModel.effective_from > date_to,
+                          SeaRouteModel.effective_to > date_to))),
         )
 
     rail_rows = await exe_q(
@@ -172,7 +161,7 @@ async def addRoute(route_to_add: AddRouteRequest):
     date_from = parse_date(route_to_add.effective_from)
     date_to = parse_date(route_to_add.effective_to)
 
-    if getattr(route_to_add.price, "drop", None):
+    if route_to_add.route_type == "rail":
         new_route = RailRouteModel(
             company_id=route_to_add.company_id,
             container_id=route_to_add.container_id,
@@ -184,7 +173,6 @@ async def addRoute(route_to_add: AddRouteRequest):
             drop=route_to_add.price.drop,
             guard=route_to_add.price.guard,
         )
-        route_type = "rail"
 
     else:
         new_route = SeaRouteModel(
@@ -197,26 +185,26 @@ async def addRoute(route_to_add: AddRouteRequest):
             filo=route_to_add.price.filo,
             fifo=route_to_add.price.fifo,
         )
-        route_type = "sea"
 
     async with database.session() as session:
         try:
             session.add(new_route)
-            await session.flush()
+            await session.flush()  # todo: try except duplicateError
 
-            route_class = RailRouteModel if route_type == "rail" else SeaRouteModel
-            get_route_stmt = select(
-                route_class,
-            ).options(
-                joinedload(route_class.container),
-                joinedload(route_class.company),
-                joinedload(route_class.start_point),
-                joinedload(route_class.end_point),
-            ).where(
-                route_class.id == new_route.id,
+            route_class = RailRouteModel if route_to_add.route_type == "rail" else SeaRouteModel
+
+            temp = await session.execute(
+                select(
+                    route_class,
+                ).where(
+                    route_class.id == new_route.id,
+                ).options(
+                    joinedload(route_class.container),
+                    joinedload(route_class.company),
+                    joinedload(route_class.start_point),
+                    joinedload(route_class.end_point),
+                )
             )
-
-            temp = await session.execute(get_route_stmt)
             joined_res = temp.scalar()
 
         except InvalidRequestError as e:
@@ -225,7 +213,7 @@ async def addRoute(route_to_add: AddRouteRequest):
                 detail=str(e)
             )
 
-    result = parseRoute(joined_res, _type=route_type)
+    result = parseRoute(joined_res, _type=route_to_add.route_type)
 
     return {
         "status": "OK",
@@ -246,15 +234,23 @@ async def editRoute(edit_route: EditRouteRequest):
             detail=f"Incorrect route type '{edit_route.edit_params.route_type}'"
         )
 
+    # If not id like this
+    correct_route_id = await exe_q(select(
+        route_class.id,
+    ).where(
+        route_class.id == edit_route.route_id,
+    ), True)
+
+    if not correct_route_id:
+        raise HTTPException(
+            status_code=404,
+            detail=f"Incorrect route id '{edit_route.route_id}'"
+        )
+
     edit_route_stmt = update(
         route_class,
     ).where(
         route_class.id == edit_route.route_id,
-    ).options(
-        joinedload(route_class.container),
-        joinedload(route_class.company),
-        joinedload(route_class.start_point),
-        joinedload(route_class.end_point),
     )
 
     route_with_current_dates = await exe_q(select(
@@ -263,128 +259,111 @@ async def editRoute(edit_route: EditRouteRequest):
         route_class.id == edit_route.route_id,
     ), True)
 
-    # Start point
-    if edit_route.edit_params.start_point_id:
-        edit_route_stmt = edit_route_stmt.values(
-            start_point_id=edit_route.edit_params.start_point_id,
-        )
+    current_date_from = route_with_current_dates.effective_from.date()
+    current_date_to = route_with_current_dates.effective_to.date()
 
-    # End point
-    if edit_route.edit_params.end_point_id:
-        edit_route_stmt = edit_route_stmt.values(
-            end_point_id=edit_route.edit_params.end_point_id,
-        )
+    update_values = {}
+    fields_to_check = ['effective_from', 'effective_to', 'container_id', 'company_id', 'start_point_id', 'end_point_id']
+    rail_price = ['price', 'drop', 'guard']
+    sea_price = ['filo', 'fifo']
 
-    # Company
-    if edit_route.edit_params.company_id:
-        edit_route_stmt = edit_route_stmt.values(
-            company_id=edit_route.edit_params.company_id,
-        )
+    date_from = parse_date(edit_route.edit_params.effective_from) if edit_route.edit_params.effective_from else datetime.fromtimestamp(0).date()
+    date_to = parse_date(edit_route.edit_params.effective_to) if edit_route.edit_params.effective_to else datetime.fromtimestamp(10000000000).date()
 
-    # Container
-    if edit_route.edit_params.container_id:
-        edit_route_stmt = edit_route_stmt.values(
-            container_id=edit_route.edit_params.container_id,
-        )
+    for field in fields_to_check:
+        value = getattr(edit_route.edit_params, field, None)
+        if value:
+            if field == 'effective_from':
+                if (not edit_route.edit_params.effective_to and date_from > current_date_to) or (edit_route.edit_params.effective_to and date_from > date_to):
+                    raise HTTPException(
+                        status_code=400,
+                        detail=f"Incorrect dates: effective_from={edit_route.edit_params.effective_from}|effective_to={edit_route.edit_params.effective_to};"
+                               f"\nCurrent dates: current_effective_from={current_date_from}|current_effective_to={current_date_to}"
+                    )
+                update_values[field] = date_from
+                continue
 
-    # Dates
-    if edit_route.edit_params.effective_from:
-        date_from = parse_date(edit_route.edit_params.effective_from)
+            if field == 'effective_to':
+                if (not edit_route.edit_params.effective_from and date_to < current_date_from) or (edit_route.edit_params.effective_from and date_to < date_from):
+                    raise HTTPException(
+                        status_code=400,
+                        detail=f"Incorrect dates: effective_from={edit_route.edit_params.effective_from}|effective_to={edit_route.edit_params.effective_to};"
+                               f"\nCurrent dates: current_effective_from={current_date_from}|current_effective_to={current_date_to}"
+                    )
+                update_values[field] = date_to
+                continue
 
-        if date_from > route_with_current_dates.effective_to:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Incorrect dates {edit_route.edit_params.effective_from} - {edit_route.edit_params.effective_to}"
-            )
+            update_values[field] = value
 
-        edit_route_stmt = edit_route_stmt.values(
-            effective_from=edit_route.edit_params.effective_from,
-        )
+    if is_rail:
+        for field in rail_price:
+            value = getattr(edit_route.edit_params.price, field, None)
+            if value:
+                update_values[field] = value
+    else:
+        for field in sea_price:
+            value = getattr(edit_route.edit_params.price, field, None)
+            if value:
+                update_values[field] = value
 
-    if edit_route.edit_params.effective_to:
-        date_to = parse_date(edit_route.edit_params.effective_to)
-
-        if date_to < route_with_current_dates.effective_from or date_from >= date_to:
-            raise HTTPException(
-                status_code=400,
-                detail=f"Incorrect dates {edit_route.edit_params.effective_from} - {edit_route.edit_params.effective_to}"
-            )
-
-        edit_route_stmt = edit_route_stmt.values(
-            effective_to=edit_route.edit_params.effective_to,
-        )
-
-    # Prices
-    if edit_route.edit_params.price:
-        if is_rail:
-            if edit_route.edit_params.price.price:
-                edit_route_stmt = edit_route_stmt.values(
-                    price=edit_route.edit_params.price.price,
-                )
-            if edit_route.edit_params.price.guard:
-                edit_route_stmt = edit_route_stmt.values(
-                    guard=edit_route.edit_params.price.guard,
-                )
-            if edit_route.edit_params.price.drop:
-                edit_route_stmt = edit_route_stmt.values(
-                    drop=edit_route.edit_params.price.drop,
-                )
-
-        if is_sea:
-            if edit_route.edit_params.price.fifo:
-                edit_route_stmt = edit_route_stmt.values(
-                    fifo=edit_route.edit_params.price.fifo,
-                )
-            if edit_route.edit_params.price.filo:
-                edit_route_stmt = edit_route_stmt.values(
-                    filo=edit_route.edit_params.price.filo,
-                )
+    edit_route_stmt = edit_route_stmt.values(**update_values)
 
     async with database.session() as session:
-        route_to_change = await session.execute(edit_route_stmt)
+        await session.execute(edit_route_stmt)
+        await session.commit()
 
-    if not route_to_change:
-        raise HTTPException(
-            status_code=400,
-            detail="Incorrect route id"
-        )
+        route_to_change = await exe_q(
+            select(
+                route_class,
+            ).where(
+                route_class.id == edit_route.route_id,
+            ).options(
+                joinedload(route_class.container),
+                joinedload(route_class.company),
+                joinedload(route_class.start_point),
+                joinedload(route_class.end_point),
+            ), True)
+
+    result = parseRoute(route_to_change, _type="rail" if is_rail else "sea")
 
     return {
         "status": "OK",
-        "changed_route": route_to_change,
+        "changed_route": result,
     }
 
 
-@router.post("/delete")
+@router.delete("/delete")
 async def deleteRoutes(ids_dict: DeleteRoutesRequest):
     async with database.session() as session:
-        rail_count_stmt = select(
-            func.count(RailRouteModel.id),
-        ).where(
-            RailRouteModel.id.in_(ids_dict.rail),
-        )
-
-        sea_count_stmt = select(
-            func.count(SeaRouteModel.id),
-        ).where(
-            SeaRouteModel.id.in_(ids_dict.sea),
-        )
-
-        count_sea = await exe_q(sea_count_stmt, True)
-        count_rail = await exe_q(rail_count_stmt, True)
-        removed_count = count_sea + count_rail
+        count_sea, count_rail = 0, 0
 
         if ids_dict.sea:
+            sea_count_stmt = select(
+                func.count(SeaRouteModel.id),
+            ).where(
+                SeaRouteModel.id.in_(ids_dict.sea),
+            )
+            count_sea = await exe_q(sea_count_stmt, True)
+
             sea_delete_stmt = delete(SeaRouteModel).where(
                 SeaRouteModel.id.in_(ids_dict.sea)
             )
             await session.execute(sea_delete_stmt)
 
         if ids_dict.rail:
+            rail_count_stmt = select(
+                func.count(RailRouteModel.id),
+            ).where(
+                RailRouteModel.id.in_(ids_dict.rail),
+            )
+            count_rail = await exe_q(rail_count_stmt, True)
+
             rail_delete_stmt = delete(RailRouteModel).where(
                 RailRouteModel.id.in_(ids_dict.rail)
             )
             await session.execute(rail_delete_stmt)
+
+    removed_count = count_sea + count_rail
 
     return {
         "status": "OK",
