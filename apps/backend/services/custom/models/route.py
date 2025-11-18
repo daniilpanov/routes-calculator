54a2634d81573232506dfa6ab345fdaccc363e6c
max1m111an <maksimilianbegunov@inbox.ru>
2025-11-19 01:24:00 +0300

    backend/custom/models/route: add DropModel
    erase drop from RailRoute

--- apps/backend/services/custom/models/route.py
+++ apps/backend/services/custom/models/route.py
@@ -1,7 +1,7 @@
 import datetime
 
 from backend.database import Base
-from sqlalchemy import DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy import DateTime, ForeignKey, String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from . import CompanyModel, ContainerModel
@@ -68,7 +68,6 @@ class RailRouteModel(Base):
     effective_to: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
 
     price: Mapped[float] = mapped_column()
-    drop: Mapped[float] = mapped_column()
     guard: Mapped[float | None] = mapped_column()
 
     start_point: Mapped[PointModel] = relationship(
@@ -76,6 +75,46 @@ class RailRouteModel(Base):
     )
     end_point: Mapped[PointModel] = relationship(
         PointModel, foreign_keys=[end_point_id]
+    )
+    company: Mapped[CompanyModel] = relationship()
+    container: Mapped[ContainerModel] = relationship()
+
+
+class DropModel(Base):
+    uid = (
+        "sea_start_point_id",
+        "sea_end_point_id",
+        "rail_start_point_id",
+        "rail_end_point_id",
+        "company_id",
+    )
+
+    __tablename__ = "drop"
+    __table_args__ = (UniqueConstraint(*uid),)
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    sea_start_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
+    sea_end_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
+    rail_start_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
+    rail_end_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
+    start_date: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    end_date: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"))
+    container_id: Mapped[int] = mapped_column(ForeignKey("containers.id"))
+    price: Mapped[float] = mapped_column(default=0)
+    currency: Mapped[str] = mapped_column(String(25))
+
+    sea_start_point: Mapped[PointModel | None] = relationship(
+        PointModel, foreign_keys=[sea_start_point_id]
+    )
+    sea_end_point: Mapped[PointModel | None] = relationship(
+        PointModel, foreign_keys=[sea_end_point_id]
+    )
+    rail_start_point: Mapped[PointModel | None] = relationship(
+        PointModel, foreign_keys=[rail_start_point_id]
+    )
+    rail_end_point: Mapped[PointModel | None] = relationship(
+        PointModel, foreign_keys=[rail_end_point_id]
     )
     company: Mapped[CompanyModel] = relationship()
     container: Mapped[ContainerModel] = relationship()
