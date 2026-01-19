dd3037dd7c0e79d4ef7f47abfa25251997e4c58b
Daniil Panov <daniil_panov2005@mail.ru>
2026-01-19 20:16:35 +0300

    back_admin: add models

---
+++ apps/back_admin/models/company.py
@@ -0,0 +1,12 @@
+from back_admin.database import Base
+from sqlalchemy import String
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class CompanyModel(Base):
+    uid = ("name",)
+
+    __tablename__ = "companies"
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    name: Mapped[str] = mapped_column(String(30), unique=True)
