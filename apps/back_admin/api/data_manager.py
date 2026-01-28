dab0975c1390362a04d588f3c0bb8416ce4d9389
Daniil Panov <daniil_panov2005@mail.ru>
2026-01-29 01:08:45 +0300

    back_admin: add db_eraser endpoint

--- apps/back_admin/api/data_manager.py
+++ apps/back_admin/api/data_manager.py
@@ -4,6 +4,7 @@ from fastapi import APIRouter
 from fastapi.responses import StreamingResponse
 
 from back_admin.service.db_dumper import create_db_dump
+from back_admin.service.db_eraser import clear_database_data
 
 router = APIRouter(prefix="/data")
 
@@ -14,3 +15,8 @@ async def make_db_dump():
         BytesIO((await create_db_dump()).encode("utf-8")),
         headers={"Content-Disposition": "attachment; filename=\"dump.sql\""},
     )
+
+
+@router.delete("/")
+async def erase_all_data():
+    await clear_database_data()
