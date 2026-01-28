f018876e94d6962d19f1ba2382b861f81c1e0baf
Daniil Panov <daniil_panov2005@mail.ru>
2026-01-29 01:08:45 +0300

    apps/back_admin: add db_eraser endpoint
    DELETE /db/data

--- apps/back_admin/api/data_manager.py
+++ apps/back_admin/api/data_manager.py
@@ -7,6 +7,7 @@ from fastapi.responses import StreamingResponse
 from starlette.status import HTTP_200_OK, HTTP_500_INTERNAL_SERVER_ERROR
 
 from back_admin.service.db_management.db_dumper import create_db_dump
+from back_admin.service.db_management.db_eraser import clear_database_data
 from shared.database import Database, get_database
 from shared.responses import DetailErrorResponse, ErrorDescriptor
 from shared.responses_fabric import create_an_error_descriptor_from_an_exception
@@ -36,6 +37,20 @@ async def make_db_dump(db: Annotated[Database, Depends(get_database)], structure
                     "Content-Disposition": f"attachment; filename=\"dump-{datetime.date.today().isoformat()}.sql\"",
                 },
             )
+        except Exception as e:
+            raise HTTPException(
+                status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+                detail=create_an_error_descriptor_from_an_exception(e),
+            ) from e
+
+
+@router.delete("/data", status_code=204, responses={
+    HTTP_500_INTERNAL_SERVER_ERROR: {"model": DetailErrorResponse[ErrorDescriptor]},
+})
+async def erase_all_data(db: Annotated[Database, Depends(get_database)]):
+    async with db.session_context() as session:
+        try:
+            await clear_database_data(session)
         except Exception as e:
             raise HTTPException(
                 status_code=HTTP_500_INTERNAL_SERVER_ERROR,
---
+++ apps/back_admin/service/db_management/db_eraser.py
@@ -0,0 +1,13 @@
+from sqlalchemy import text
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
+async def clear_database_data(session: AsyncSession):
+    await session.execute(text("SET FOREIGN_KEY_CHECKS = 0"))
+
+    tables_descriptor = (await session.execute(text("SHOW TABLES"))).scalars()
+
+    for table in tables_descriptor:
+        await session.execute(text(f"DELETE FROM `{table}`"))
+
+    await session.execute(text("SET FOREIGN_KEY_CHECKS = 1"))
