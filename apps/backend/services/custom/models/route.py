d7730b720dc0265d0483648f003e60206c50a102
Daniil Panov <daniil_panov2005@mail.ru>
2026-02-12 12:19:34 +0300

    backend/custom/models/route: fix 'effective_from' and 'effective_to' props

--- apps/backend/services/custom/models/route.py
+++ apps/backend/services/custom/models/route.py
@@ -69,8 +69,8 @@ class RouteModel(Base):
     company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"))
     start_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"))
     end_point_id: Mapped[int] = mapped_column(ForeignKey("points.id"))
-    effective_from: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
-    effective_to: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    effective_from: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False))
+    effective_to: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False))
     comment: Mapped[str | None] = mapped_column(String(255), nullable=True, default=None)
 
     type: Mapped[RouteTypeEnum] = mapped_column(  # noqa: A003
@@ -109,8 +109,8 @@ class DropModel(Base):
     sea_end_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
     rail_start_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
     rail_end_point_id: Mapped[int | None] = mapped_column(ForeignKey("points.id"), nullable=True)
-    start_date: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
-    end_date: Mapped[datetime.date] = mapped_column(DateTime(timezone=False))
+    start_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False))
+    end_date: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=False))
     company_id: Mapped[int] = mapped_column(ForeignKey("companies.id"))
     container_id: Mapped[int] = mapped_column(ForeignKey("containers.id"))
     price: Mapped[float] = mapped_column(default=0)
