ecef9bacb3ccf411cbe5982c4387c91976480987
Daniil Panov <daniil_panov2005@mail.ru>
2026-02-16 08:27:45 +0300

    shared/models: split 'route.py' and 'DropModel'

--- apps/shared/models/__init__.py
+++ apps/shared/models/__init__.py
@@ -1,4 +1,5 @@
 from .company import CompanyModel
 from .container import ContainerModel, ContainerType
+from .drop import DropModel
 from .point import PointModel
-from .route import DropModel, PriceModel, PriceTypeEnum, RouteModel, RouteTypeEnum
+from .route import PriceModel, PriceTypeEnum, RouteModel, RouteTypeEnum
