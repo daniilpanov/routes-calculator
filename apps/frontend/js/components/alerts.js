feb9a493e2418e7b9ceb0f99cd3aef4b9989bcd6
Daniil Panov <daniil_panov2005@mail.ru>
2026-02-17 00:32:22 +0300

    frontend/js/alerts: improve - replace all '\n' to '<p>'

--- apps/frontend/js/components/alerts.js
+++ apps/frontend/js/components/alerts.js
@@ -1,14 +1,15 @@
 function showGlobalAlert(message, type = "danger") {
     const container = document.getElementById("alertContainer");
     container.innerHTML = `<div class="alert alert-${type} alert-fixed alert-dismissible fade show" role="alert">
-      ${message}
+      ${message.replaceAll("\n", "<p>")}
       <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Закрыть"></button>
     </div>`;
     container.classList.remove("d-none");
 
     setTimeout(() => {
         const alert = container.querySelector(".alert");
-        if (alert) new bootstrap.Alert(alert).close();
+        if (alert)
+            new bootstrap.Alert(alert).close();
     }, 3000);
     console.log(type, "-", message);
 }
