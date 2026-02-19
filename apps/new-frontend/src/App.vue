6798fc86987f8c9de41ea6c3006277836a37caa9
Daniil Panov <daniil_panov2005@mail.ru>
2026-02-20 01:41:42 +0300

    [TEMP] apps/new-frontend: setup routing
    TODO: move stores initialization to services

--- apps/new-frontend/src/App.vue
+++ apps/new-frontend/src/App.vue
@@ -1,11 +1,94 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import type { ITranslatePoint } from "@/interfaces/Point";
+
+import { useRates } from "@/stores/rates";
+import { usePoints } from "@/stores/points";
+
+const { setRates, setCurrentRate } = useRates();
+const { setPoints } = usePoints();
+
+setPoints([
+    {
+        id: 1,
+        translates: new Map<string, ITranslatePoint>([
+            [
+                "ru",
+                {
+                    entityId: 1,
+                    lang: "ru",
+                    country: "test1",
+                    name: "test1",
+                },
+            ],
+        ]),
+        ports: [],
+        companies: [{ id: 123, name: "tetetet" }],
+    },
+    {
+        id: 2,
+        translates: new Map<string, ITranslatePoint>([
+            [
+                "ru",
+                {
+                    entityId: 2,
+                    lang: "ru",
+                    country: "test2",
+                    name: "test2",
+                },
+            ],
+        ]),
+        ports: [],
+        companies: [],
+    },
+    {
+        id: 3,
+        translates: new Map<string, ITranslatePoint>([
+            [
+                "ru",
+                {
+                    entityId: 3,
+                    lang: "ru",
+                    country: "test3",
+                    name: "test3",
+                },
+            ],
+        ]),
+        ports: [],
+        companies: [],
+    },
+]);
+
+setRates({
+    RUB: 1,
+    USD: 2,
+    EUR: 3,
+    CNY: 1,
+    AUD: 6,
+    BUG: 1321,
+});
+setCurrentRate("RUB");
+</script>
 
 <template>
-    <h1>You did it!</h1>
-    <p>
-        Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-        documentation
-    </p>
+    <main class="container">
+        <div class="alert alert-warning d-flex align-items-center" role="alert">
+            <svg
+                class="bi flex-shrink-0 me-2"
+                width="24"
+                height="24"
+                role="img"
+                aria-label="Warning:"
+            >
+                <use xlink:href="#exclamation-triangle-fill"></use>
+            </svg>
+            <div>
+                Сервис находится на стадии бета-тестирования. Некоторые функции могут работать
+                нестабильно. Пожалуйста, сообщите нам, если заметите ошибку.
+            </div>
+        </div>
+
+        <router-view />
+    </main>
 </template>
 
 <style scoped></style>
--- apps/new-frontend/src/router/index.ts
+++ apps/new-frontend/src/router/index.ts
@@ -1,8 +1,44 @@
+import CalculatorPage from "@/pages/CalculatorPage.vue";
+
 import { createRouter, createWebHistory } from "vue-router";
+
+const parseCalculatorQueryParams = (query: Record<string, unknown>) => {
+    const params: Record<string, unknown> = {};
+
+    if (query.currency) params.currency = query.currency;
+
+    if (query.date && !Number.isNaN(new Date(query.date as string).getDate()))
+        params.date = query.date;
+
+    if (query.showAllRoutes) params.showAllRoutes = query.showAllRoutes === "true";
+
+    if (query.departureId) {
+        const num = Number(query.departureId);
+        params.departureId = isNaN(num) ? query.departureId : num;
+
+        if (query.destinationId) {
+            const num = Number(query.destinationId);
+            params.destinationId = isNaN(num) ? query.destinationId : num;
+        }
+    }
+
+    if (query.type) params.containerType = Number(query.type);
+
+    if (query.weight) params.containerWeight = Number(query.weight);
+
+    return params;
+};
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [],
+    routes: [
+        {
+            path: "/",
+            name: "calculator",
+            component: CalculatorPage,
+            props: (route) => parseCalculatorQueryParams(route.query),
+        },
+    ],
 });
 
 export default router;
