bbfd717035666ad1e0dda7343442561b90009a1e
Daniil Panov <daniil_panov2005@mail.ru>
2026-03-03 20:28:18 +0300

    apps/backend/api/v1/rates: add ["RUB", "RUR", russian-equivalent] = 1 to rates list

--- apps/backend/services/custom/get_rates.py
+++ apps/backend/services/custom/get_rates.py
@@ -2,6 +2,7 @@ import datetime
 
 from pycbrf.toolbox import ExchangeRates
 
+# Cache must be invalidated every day
 _rates: dict[str, float] = {}
 _last_update = None
 
@@ -17,5 +18,10 @@ def get_rates(dt_now: datetime.datetime | None = None):
 
     rates = ExchangeRates(dt_now)
     _rates = {currency.code: float(currency.value) for currency in rates.rates}
+    # _rates["RUB"] = 1 because RUB == RUB
+    _rates["RUB"] = 1
+    _rates["RUR"] = 1
+    _rates["РУБ"] = 1
+
     _last_update = dt_now.date()
     return _rates
