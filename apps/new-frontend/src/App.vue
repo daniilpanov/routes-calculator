61e6eb6467d540072f7737baf3fcb19ea127f1a8
Daniil Panov <daniil_panov2005@mail.ru>
2026-03-04 01:45:34 +0300

    apps/new-frontend: add 'ThemeSwitcher' realisation

--- apps/new-frontend/src/App.vue
+++ apps/new-frontend/src/App.vue
@@ -1,4 +1,14 @@
 <script setup lang="ts">
+import ThemeSwitcher from "@/widgets/ThemeSwitcher.vue";
+
+import { getCurrentTheme, setCurrentTheme, Theme } from "@/services/theme";
+import { ref, watch } from "vue";
+
+const theme = ref<Theme>(getCurrentTheme());
+watch(theme, (newTheme: Theme) => {
+    document.documentElement.setAttribute("data-bs-theme", newTheme);
+    setCurrentTheme(theme.value);
+}, { immediate: true });
 </script>
 
 <template>
@@ -13,6 +23,8 @@
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </symbol>
     </svg>
+
+    <ThemeSwitcher v-model="theme" />
 
     <main class="container">
         <div class="alert alert-warning d-flex align-items-center" role="alert">
