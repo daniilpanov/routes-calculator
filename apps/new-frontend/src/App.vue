f1365177ba303d428981d39b173c7d72d8af0d24
Daniil Panov <daniil_panov2005@mail.ru>
2026-03-04 16:05:24 +0300

    apps/new-frontend: add 'HeaderComponent' realisation

--- apps/new-frontend/src/App.vue
+++ apps/new-frontend/src/App.vue
@@ -1,11 +1,15 @@
 <script setup lang="ts">
+import HeaderComponent from "@/components/HeaderComponent.vue";
 import ThemeSwitcher from "@/widgets/ThemeSwitcher.vue";
 
 import { lockRates, updateRates } from "@/services/rates";
+import { useRates } from "@/stores/rates";
 import { getCurrentTheme, setCurrentTheme, Theme } from "@/services/theme";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
 
 lockRates(updateRates());
+
+const rates = computed(() => useRates().rates);
 
 const theme = ref<Theme>(getCurrentTheme());
 watch(theme, (newTheme: Theme) => {
@@ -26,6 +30,10 @@ watch(theme, (newTheme: Theme) => {
             <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
         </symbol>
     </svg>
+
+    <header>
+        <HeaderComponent :rates="rates" />
+    </header>
 
     <ThemeSwitcher v-model="theme" />
 
---
+++ apps/new-frontend/src/components/HeaderComponent.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+import { computed } from "vue";
+import type { RatesMap } from "@/stores/rates";
+
+const props = defineProps<{ rates: RatesMap }>();
+
+const USD = computed(() => props.rates.get("USD"));
+const EUR = computed(() => props.rates.get("EUR"));
+const CNY = computed(() => props.rates.get("CNY"));
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">Калькулятор</a>
+
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup"></div>
+            <div class="navbar-nav"></div>
+
+            <div class="header-rates">
+                <div v-if="USD">
+                    <span class="symbol">$</span> {{ USD }}
+                </div>
+                <div v-if="EUR">
+                    <span class="symbol">€</span> {{ EUR }}
+                </div>
+                <div v-if="CNY">
+                    <span class="symbol">¥</span> {{ CNY }}
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<style scoped>
+.header-rates .symbol {
+    font-size: larger;
+    color: var(--bs-blue);
+}
+</style>
