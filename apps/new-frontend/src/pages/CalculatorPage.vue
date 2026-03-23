9f8b893cff6cd7d120cd1a1122535255802ed605
Daniil Panov <daniil_panov2005@mail.ru>
2026-03-23 12:31:20 +0300

    apps/new-frontend/pages/Calculator: auto disable edit mode on 'saveInPDF' button click

--- apps/new-frontend/src/pages/CalculatorPage.vue
+++ apps/new-frontend/src/pages/CalculatorPage.vue
@@ -127,8 +127,12 @@ function reset() {
 }
 
 async function saveInPdf() {
+    editMode.value = false;
+    await nextTick();
+
     printMode.value = true;
     await nextTick();
+
     window.print();
     printMode.value = false;
 }
@@ -188,7 +192,7 @@ onMounted(() => {
             </template>
         </button>
 
-        <button class="btn btn-success" :disabled="!routesRef || editMode" @click="saveInPdf">Сохранить результат в PDF</button>
+        <button class="btn btn-success" :disabled="!routesRef" @click="saveInPdf">Сохранить результат в PDF</button>
     </div>
 
     <hr />
