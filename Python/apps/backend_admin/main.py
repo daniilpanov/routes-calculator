c6417542c43ca4ea6dc61f3ba55d94b537de9b90
Daniil Panov <daniil_panov2005@mail.ru>
2026-04-30 18:17:16 +0300

    Python/backend_admin/main: make some imports relative

--- Python/apps/backend_admin/main.py
+++ Python/apps/backend_admin/main.py
@@ -3,12 +3,12 @@ from importlib.util import find_spec
 from fastapi import FastAPI
 from starlette.requests import Request
 from starlette.responses import JSONResponse
-
-from backend_admin.autodiscover import api_discover
-from backend_admin.config import get_settings
-from backend_admin.jwt_middleware import JWTAuthMiddleware
 from fastapi_another_jwt_auth import AuthJWT
 from fastapi_another_jwt_auth.exceptions import AuthJWTException
+
+from .autodiscover import api_discover
+from .config import get_settings
+from .jwt_middleware import JWTAuthMiddleware
 
 if find_spec("dotenv") is not None:
     from dotenv import load_dotenv
