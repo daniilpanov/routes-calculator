81b7137102525d42bb22f0627eee5b2293331573
Daniil Panov <daniil_panov2005@mail.ru>
2026-06-10 02:51:22 +0300

    Python: Enable F401 rule in flake8

--- Python/apps/module_data_internal/aggregators/__init__.py
+++ Python/apps/module_data_internal/aggregators/__init__.py
@@ -1,3 +1,3 @@
-from .containers import get_containers, search_container_ids
-from .points import get_departure_points, get_destination_points
-from .routes import find_all_paths
+from .containers import get_containers, search_container_ids  # noqa: F401
+from .points import get_departure_points, get_destination_points  # noqa: F401
+from .routes import find_all_paths  # noqa: F401
--- Python/apps/module_data_internal/schemas/__init__.py
+++ Python/apps/module_data_internal/schemas/__init__.py
@@ -1,12 +1,12 @@
-from module_shared.database import Base
+from module_shared.database import Base  # noqa: F401
 
-from .company import CompanyModel
-from .container import ContainerModel, ContainerType
-from .drop import DropModel
-from .point import PointModel
-from .service import ServiceModel
+from .company import CompanyModel  # noqa: F401
+from .container import ContainerModel, ContainerType  # noqa: F401
+from .drop import DropModel  # noqa: F401
+from .point import PointModel  # noqa: F401
+from .service import ServiceModel  # noqa: F401
 
-from .route import (  # isort:skip
+from .route import (  # isort:skip  # noqa: F401
     ContainerOwner,
     ContainerShipmentTerms,
     ContainerTransferTerms,
