4fc61b7b54fe4937f4d07a85abc9639f4cb830f0
Daniil Panov <daniil_panov2005@mail.ru>
2026-06-23 19:18:26 +0300

    Database: Add settings table

--- Python/apps/module_shared/schemas/__init__.py
+++ Python/apps/module_shared/schemas/__init__.py
@@ -1,5 +1,6 @@
 from module_shared.database import Base
 
 from .demo_guest import DemoGuestModel
+from .setting import SettingModel, SettingType
 
-__all__ = ["Base", "DemoGuestModel"]
+__all__ = ["Base", "DemoGuestModel", "SettingModel", "SettingType"]
---
+++ Python/apps/module_shared/schemas/setting.py
@@ -0,0 +1,35 @@
+import enum
+
+from module_shared.database import Base
+from sqlalchemy import Enum, Text, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class SettingType(enum.Enum):
+    BOOL = "BOOL"
+    INT = "INT"
+    FLOAT = "FLOAT"
+    STRING = "STRING"
+    JSON = "JSON"
+
+
+class SettingModel(Base):
+    uid = ("group", "name")
+
+    __tablename__ = "settings"
+    __table_args__ = (UniqueConstraint(*uid, name="uk__setting_group_name"),)
+
+    id: Mapped[int] = mapped_column(primary_key=True)  # noqa: A003
+    group: Mapped[str] = mapped_column(Text, nullable=False)
+    name: Mapped[str] = mapped_column(Text, nullable=False)
+    description: Mapped[str | None] = mapped_column(Text, nullable=True)
+    value_type: Mapped[SettingType] = mapped_column(
+        Enum(
+            SettingType,
+            create_constraint=True,
+            check_constraint=True,
+            validate_strings=True,
+        ),
+        nullable=False,
+    )
+    value: Mapped[str | None] = mapped_column(Text, nullable=True)
